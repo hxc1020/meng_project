fc937cafe6a4bf631fca6a522eae44fd740fbd3e
hxc <ringers.stewed-07@icloud.com>
2023-03-25 15:47:11 +0800

    bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meng_project/bar.dart';
+
+import 'colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,19 +16,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: _buildMengTheme(),
+      home: const MyHomePage(title: '朦的项目管理'),
+    );
+  }
+
+  ThemeData _buildMengTheme() {
+    final base = ThemeData.dark();
+    return ThemeData(
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        backgroundColor: MengColors.primaryBackground,
+        elevation: 0,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      scaffoldBackgroundColor: MengColors.primaryBackground,
+      focusColor: MengColors.focusColor,
+      inputDecorationTheme: const InputDecorationTheme(
+        labelStyle: TextStyle(
+          color: MengColors.gray,
+          fontWeight: FontWeight.w500,
+        ),
+        filled: true,
+        fillColor: MengColors.inputBackground,
+        focusedBorder: InputBorder.none,
+      ),
+      visualDensity: VisualDensity.standard,
+      colorScheme: base.colorScheme.copyWith(
+        primary: MengColors.primaryBackground,
+      ),
     );
   }
 }
@@ -74,37 +93,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.add), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.notifications), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: const HomePage(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
---
+++ lib/projects.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Projects extends StatelessWidget {
+  final String title;
+
+  const Projects({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        Text("tom"),
+        Text("tom"),
+        Text("tom"),
+        Text("tom"),
+        Text("tom"),
+        Text("tom"),
+      ],
+    );
+  }
+}
